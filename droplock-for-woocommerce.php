a3400730d3620ef6e94022fb426bc6f4a68a9c82
DropLock Setup <setup@droplockwp.com>
2026-05-23 20:53:13 -0700

    chore: define DROPLOCK_PRO_VERSION so the free plugin can detect Pro and stand down

--- droplock-for-woocommerce.php
+++ droplock-for-woocommerce.php
@@ -23,6 +23,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 define( 'DROPLOCK_VERSION', '1.0.0' );
+define( 'DROPLOCK_PRO_VERSION', '1.0.0' );
 define( 'DROPLOCK_PLUGIN_FILE', __FILE__ );
 define( 'DROPLOCK_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 define( 'DROPLOCK_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
